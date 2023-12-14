432d4123ce108eac9472a995b52831d263a5a89b
eva2526 <148702094+eva2526@users.noreply.github.com>
2023-12-14 09:40:46 +0100

    script for simple to do list using jQuery

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # To-Do-List-App
  simple App using jQuery for maintaining a to do list
+
+## Features
+* User can add a new item to a list of items
+* User can cross out an item from the list of items
+* User can delete an item from the list of items
+* User can sort the items per drag and drop
---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+function newItem() {
+  //reading value from input field and adding new item to list
+  let newLi = $('<li></li>');
+  let inputText = $('input').val();
+  newLi.append(inputText);
+
+  //checking if value is empty 
+  if (inputText === '') {
+    alert("You must write something!");
+  } else {
+    $('#list').append(newLi);
+    $('#input').val(""); // Reset input field
+  }
+
+  //crossing out an item from list
+  function crossOut() {
+    newLi.toggleClass("strike");
+  }
+  newLi.on("dblclick", crossOut);
+
+  //adding delete button X
+  let crossOutButton = $('<button class="crossOutButton">X</button>');
+  newLi.append(crossOutButton);
+  function deleteListItem() {
+    newLi.addClass("delete");
+  }
+  crossOutButton.on("click", deleteListItem);
+
+}
+
+// make list sortable
+$('#list').sortable();
+
+//add list item on hitting enter key & prevent submission of form
+$(document).on('keydown', function (event) {
+  let keycode = (event.keyCode ? event.keyCode : event.which);
+  if (keycode == '13') {
+    event.preventDefault();
+    newItem();
+  }
+});
